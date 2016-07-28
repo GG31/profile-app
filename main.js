b1cdb8892a465c94edf77b351c8c7015d5ef2dac
Geneviève Cirera <genevieve.cirera@gmail.com>
2016-07-28 14:36:28 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var path = require('path');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+var server = require("http").createServer(app);
+
+app.get('/', function(req, res) {
+   res.sendFile(path.join(__dirname, '/views', 'index.html'));
+});
+
+server.listen(8081);
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
